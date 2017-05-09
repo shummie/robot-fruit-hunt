0e03fb5b9d989debf0876fa25b453656ea38d52c
Raymond Shum <rbshum@gmail.com>
2017-05-08 21:17:41 -0500

    changed explicit namespace references to this.

--- bots/randombot.js
+++ bots/randombot.js
@@ -1,37 +1,37 @@
 var RandomBot = {
         newGame: function(playerNumber) {
-        
+
         if (playerNumber === undefined) {
-            RandomBot.playerNumber = 1
+            this.playerNumber = 1
         } else {
-            RandomBot.playerNumber = playerNumber
+            this.playerNumber = playerNumber
         }
-        
+
     },
     makeMove: function() {
        // to disable to opponent, uncomment the next line
        // return PASS;
 
-       RandomBot.board = get_board();
-       
-       if (RandomBot.playerNumber == 1) {
-           RandomBot.x = get_my_x();
-           RandomBot.y = get_my_y();
-           RandomBot.ox = get_opponent_x();
-           RandomBot.oy = get_opponent_y();
+       this.board = get_board();
+
+       if (this.playerNumber == 1) {
+           this.x = get_my_x();
+           this.y = get_my_y();
+           this.ox = get_opponent_x();
+           this.oy = get_opponent_y();
        } else {
-           RandomBot.ox = get_my_x();
-           RandomBot.oy = get_my_y();
-           RandomBot.x = get_opponent_x();
-           RandomBot.y = get_opponent_y();
+           this.ox = get_my_x();
+           this.oy = get_my_y();
+           this.x = get_opponent_x();
+           this.y = get_opponent_y();
        }
 
        // we found an item! take it!
-       if (has_item(RandomBot.board[RandomBot.x][RandomBot.y])) {
+       if (has_item(this.board[this.x][this.y])) {
            return TAKE;
        }
-       
-       
+
+
        var rand = Math.random() * 4;
 
        if (rand < 1) return NORTH;
--- bots/simplebot.js
+++ bots/simplebot.js
@@ -1,93 +1,93 @@
 var SimpleBot = {
     newGame: function(playerNumber) {
-        
+
         if (playerNumber === undefined) {
-            SimpleBot.playerNumber = 1
+            this.playerNumber = 1
         } else {
-            SimpleBot.playerNumber = playerNumber
+            this.playerNumber = playerNumber
         }
-        
+
     },
     makeMove: function() {
        // to disable to opponent, uncomment the next line
        // return PASS;
 
-       SimpleBot.board = get_board();
-       
-       if (SimpleBot.playerNumber == 1) {
-           SimpleBot.x = get_my_x();
-           SimpleBot.y = get_my_y();
-           SimpleBot.ox = get_opponent_x();
-           SimpleBot.oy = get_opponent_y();
+       this.board = get_board();
+
+       if (this.playerNumber == 1) {
+           this.x = get_my_x();
+           this.y = get_my_y();
+           this.ox = get_opponent_x();
+           this.oy = get_opponent_y();
        } else {
-           SimpleBot.ox = get_my_x();
-           SimpleBot.oy = get_my_y();
-           SimpleBot.x = get_opponent_x();
-           SimpleBot.y = get_opponent_y();
+           this.ox = get_my_x();
+           this.oy = get_my_y();
+           this.x = get_opponent_x();
+           this.y = get_opponent_y();
        }
 
        // we found an item! take it!
-       if (has_item(SimpleBot.board[SimpleBot.x][SimpleBot.y])) {
+       if (has_item(this.board[this.x][this.y])) {
            return TAKE;
        }
 
        // looks like we'll have to keep track of what moves we've looked at
-       SimpleBot.toConsider = new Array();
-       SimpleBot.considered = new Array(HEIGHT);
+       this.toConsider = new Array();
+       this.considered = new Array(HEIGHT);
        for (var i = 0; i < WIDTH; i++) {
-           SimpleBot.considered[i] = new Array(HEIGHT);
+           this.considered[i] = new Array(HEIGHT);
            for (var j = 0; j < HEIGHT; j++) {
-               SimpleBot.considered[i][j] = 0;
+               this.considered[i][j] = 0;
            }
        }
 
        // let's find the move that will start leading us to the closest item
-       return SimpleBot.findMove(new node(SimpleBot.x, SimpleBot.y, -1));
+       return this.findMove(new node(this.x, this.y, -1));
     },
 
     findMove: function(n) {
        // closest item! we will go to it
-       if (has_item(SimpleBot.board[n.x][n.y]))
+       if (has_item(this.board[n.x][n.y]))
            return n.move;
 
        var possibleMove = n.move;
 
        // NORTH
-       if (SimpleBot.considerMove(n.x, n.y-1)) {
+       if (this.considerMove(n.x, n.y-1)) {
            if (n.move == -1) {
                possibleMove = NORTH;
-           } 
-           SimpleBot.toConsider.push(new node(n.x, n.y-1, possibleMove));
-       } 
+           }
+           this.toConsider.push(new node(n.x, n.y-1, possibleMove));
+       }
 
        // SOUTH
-       if (SimpleBot.considerMove(n.x, n.y+1)) {
+       if (this.considerMove(n.x, n.y+1)) {
            if (n.move == -1) {
                possibleMove = SOUTH;
-           } 
-           SimpleBot.toConsider.push(new node(n.x, n.y+1, possibleMove));
-       } 
+           }
+           this.toConsider.push(new node(n.x, n.y+1, possibleMove));
+       }
 
        // WEST
-       if (SimpleBot.considerMove(n.x-1, n.y)) {
+       if (this.considerMove(n.x-1, n.y)) {
            if (n.move == -1) {
                possibleMove = WEST;
-           } 
-           SimpleBot.toConsider.push(new node(n.x-1, n.y, possibleMove));
-       } 
+           }
+           this.toConsider.push(new node(n.x-1, n.y, possibleMove));
+       }
 
-       // EAST 
-       if (SimpleBot.considerMove(n.x+1, n.y)) {
+       // EAST
+       if (this.considerMove(n.x+1, n.y)) {
            if (n.move == -1) {
                possibleMove = EAST;
-           } 
-           SimpleBot.toConsider.push(new node(n.x+1, n.y, possibleMove));
-       } 
+           }
+           this.toConsider.push(new node(n.x+1, n.y, possibleMove));
+       }
 
        // take next node to bloom out from
-       if (SimpleBot.toConsider.length > 0) {
-           var next = SimpleBot.toConsider.shift();
-           return SimpleBot.findMove(next);
+       if (this.toConsider.length > 0) {
+           var next = this.toConsider.shift();
+           return this.findMove(next);
        }
 
        // no move found
@@ -95,9 +95,9 @@ var SimpleBot = {
     },
 
     considerMove: function(x, y) {
-       if (!SimpleBot.isValidMove(x, y)) return false;
-       if (SimpleBot.considered[x][y] > 0) return false;
-       SimpleBot.considered[x][y] = 1;
+       if (!this.isValidMove(x, y)) return false;
+       if (this.considered[x][y] > 0) return false;
+       this.considered[x][y] = 1;
        return true;
     },
 
